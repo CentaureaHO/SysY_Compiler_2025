316f51c0fdbdbf4dd56a68595ade27fcf9043aaa
王禹衡 <837880374@qq.com>
2024-10-08 18:58:43 +0800

    添加测试样例

---
+++ test.py
@@ -0,0 +1,32 @@
+import sys
+import subprocess
+import os
+
+def handle_lexer():
+    bin_path = 'testcase/build/2_lexicalAnalyzer/lexAnalyzer'
+    if not os.path.exists(bin_path):
+        make_lexer = subprocess.run(["make", "testLexicalAnalysis"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(make_lexer.stdout.decode("utf-8"))
+    syfiles_path = 'testcase/case/2_lexicalAnalyzer'
+    syfiles = [f for f in os.listdir(syfiles_path) if f.endswith('.sy')]
+    for file in syfiles:
+        print(f'Running {file}...')
+        subprocess.run([bin_path, f'{syfiles_path}/{file}'])
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 test.py <command>")
+        print("Commands:")
+        print("  lexer")
+        sys.exit(1)
+
+    command = sys.argv[1]
+
+    if command == "lexer":
+        handle_lexer()
+    else:
+        print(f"Unknown command: {command}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
---
+++ testcase/source/2_lexicalAnalyzer/lexAnalyzer.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <parser/token_parser.h>
+using namespace std;
+
+#define STR_PW 30
+#define INT_PW 8
+#define MIN_GAP 5
+#define STR_REAL_WIDTH (STR_PW - MIN_GAP)
+
+string truncateString(const string& str, size_t width)
+{
+    if (str.length() > width) return str.substr(0, width - 3) + "...";
+    return str;
+}
+
+string getOutputFileName(const string& inputFile)
+{
+    string outputFile = inputFile;
+    size_t pos        = outputFile.find(".sy");
+    if (pos != string::npos) { outputFile = outputFile.substr(0, pos); }
+    outputFile += ".token";
+    return outputFile;
+}
+
+int main(int argc, char* argv[])
+{
+    istream* inStream  = &cin;
+    ostream* outStream = &cout;
+
+    ifstream file;
+    ofstream outFile;
+
+    if (argc > 1)
+    {
+        string inputFile = argv[1];
+        file.open(inputFile);
+
+        if (!file)
+        {
+            cerr << "Error: Cannot open input file " << inputFile << endl;
+            return 1;
+        }
+
+        inStream = &file;
+
+        string outputFile = getOutputFileName(inputFile);
+        outFile.open(outputFile);
+
+        if (!outFile)
+        {
+            cerr << "Error: Cannot open output file " << outputFile << endl;
+            return 1;
+        }
+
+        outStream = &outFile;
+    }
+
+    TokenParser parser(*inStream);
+
+    *outStream << left;
+    *outStream << setw(STR_PW) << "Token" << setw(STR_PW) << "Lexeme" << setw(STR_PW) << "Property"
+               << setw(INT_PW) << "Line" << setw(INT_PW) << "Column" << endl;
+
+    for (auto& token : parser.tokens)
+    {
+        *outStream << setw(STR_PW) << truncateString(getName(get<0>(token)), STR_REAL_WIDTH)  // Token
+                   << setw(STR_PW) << truncateString(get<1>(token), STR_REAL_WIDTH)           // Lexeme
+                   << setw(STR_PW) << truncateString(get<2>(token), STR_REAL_WIDTH)           // Property
+                   << setw(INT_PW) << get<3>(token)                                           // Line
+                   << setw(INT_PW) << get<4>(token)                                           // Column
+                   << endl;
+    }
+
+    if (file.is_open()) file.close();
+    if (outFile.is_open()) outFile.close();
+
+    return 0;
+}
