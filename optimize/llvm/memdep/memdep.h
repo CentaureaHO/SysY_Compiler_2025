d1fcc9f07dc73dab4d2c500917056dca59128f47
CentaureaHO <tangxianda15@outlook.com>
2025-07-11 15:58:14 +0800

    MemorySSA for mem dep

--- optimize/llvm/memdep/memdep.h
+++ optimize/llvm/memdep/memdep.h
@@ -7,34 +7,115 @@
 #include "llvm/alias_analysis/alias_analysis.h"
 #include <map>
 #include <vector>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
 
 namespace Analysis
 {
+    class MemoryDef;
+    class MemoryUse;
+    class MemoryPhi;
+    class MemoryAccess;
+
+    class MemorySSA
+    {
+      public:
+        enum MemoryAccessType
+        {
+            DEF,
+            USE,
+            PHI
+        };
+
+        struct MemoryLocation
+        {
+            LLVMIR::Operand* ptr;
+            LLVMIR::DataType size;
+            bool             operator<(const MemoryLocation& other) const
+            {
+                if (ptr != other.ptr) return ptr < other.ptr;
+                return size < other.size;
+            }
+        };
+
+        class MemoryAccess
+        {
+          public:
+            MemoryAccessType           type;
+            LLVMIR::Instruction*       inst;
+            MemoryLocation             location;
+            int                        block_id;
+            int                        inst_id;
+            MemoryAccess*              defining_access;
+            std::vector<MemoryAccess*> uses;
+
+            MemoryAccess(MemoryAccessType t, LLVMIR::Instruction* i = nullptr)
+                : type(t), inst(i), block_id(-1), inst_id(-1), defining_access(nullptr)
+            {}
+        };
+
+        class MemoryDef : public MemoryAccess
+        {
+          public:
+            MemoryDef(LLVMIR::Instruction* i) : MemoryAccess(DEF, i) {}
+        };
+
+        class MemoryUse : public MemoryAccess
+        {
+          public:
+            MemoryUse(LLVMIR::Instruction* i) : MemoryAccess(USE, i) {}
+        };
+
+        class MemoryPhi : public MemoryAccess
+        {
+          public:
+            std::vector<std::pair<int, MemoryAccess*>> incoming_values;
+            MemoryPhi(int bb_id) : MemoryAccess(PHI, nullptr) { block_id = bb_id; }
+        };
+
+      private:
+        LLVMIR::IR*    ir;
+        AliasAnalyser* alias_analyser;
+
+        std::map<CFG*, std::map<int, std::vector<MemoryAccess*>>>            bb_memory_accesses;
+        std::map<CFG*, std::map<int, MemoryPhi*>>                            bb_memory_phis;
+        std::map<CFG*, std::map<MemoryLocation, std::vector<MemoryAccess*>>> location_accesses;
+        std::map<LLVMIR::Instruction*, MemoryAccess*>                        inst_to_access;
+
+        void           buildMemorySSA(CFG* cfg);
+        void           insertMemoryPhis(CFG* cfg);
+        void           renameMemoryAccesses(CFG* cfg);
+        void           processBlock(CFG* cfg, int bb_id, std::map<MemoryLocation, MemoryAccess*>& current_def);
+        MemoryLocation getMemoryLocation(LLVMIR::Instruction* inst);
+        bool           mayAlias(const MemoryLocation& loc1, const MemoryLocation& loc2, CFG* cfg);
+        MemoryAccess*  getDefiningAccess(LLVMIR::Instruction* inst, CFG* cfg);
+
+      public:
+        MemorySSA(LLVMIR::IR* ir, AliasAnalyser* aa);
+        void run();
+        bool isLoadSameMemory(LLVMIR::Instruction* inst1, LLVMIR::Instruction* inst2, CFG* cfg);
+        bool isDepend(LLVMIR::Instruction* inst1, LLVMIR::Instruction* inst2, CFG* cfg);
+        bool haveMemAccessBetween(LLVMIR::Instruction* start, LLVMIR::Instruction* end, CFG* cfg);
+    };
+
     class MemoryDependenceAnalyser
     {
       private:
-        LLVMIR::IR*                                                      ir;
-        AliasAnalyser*                                                   alias_analyser;
-        std::map<CFG*, std::vector<LLVMIR::Instruction*>>                cfg_store_inst_map;
-        std::map<CFG*, std::map<int, std::vector<LLVMIR::Instruction*>>> result_used_map;
+        LLVMIR::IR* ir;
+        MemorySSA*  memory_ssa;
 
       public:
         MemoryDependenceAnalyser(LLVMIR::IR* ir, AliasAnalyser* aa);
-        ~MemoryDependenceAnalyser() = default;
+        ~MemoryDependenceAnalyser();
 
         void setIR(LLVMIR::IR* ir) { this->ir = ir; }
 
         void run();
 
-        // return if I2 depends on I1(if I2 is after I1 in one BB)
-        // now we only consider two memory access instructions
         bool isDepend(LLVMIR::Instruction* inst1, LLVMIR::Instruction* inst2, CFG* cfg);
-
         bool haveMemAccessBetween(LLVMIR::Instruction* start, LLVMIR::Instruction* end, CFG* cfg);
-
         bool isLoadSameMemory(LLVMIR::Instruction* inst1, LLVMIR::Instruction* inst2, CFG* cfg);
-
-        void memDepTest();
     };
 }  // namespace Analysis
 
